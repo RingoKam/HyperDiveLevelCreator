84fac56f87fdbb3e99be77f88624ef7b635aaa53
ringo kam <ringo_kam@hotmail.com>
2020-11-17 13:53:18 -0500

    getting started

---
+++ levelcreator/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content">
+    <div class="content-section">
+      <textarea>
+      </textarea>
+    </div>
+    <div class="content-section">
+      <LevelCreator />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LevelCreator from "./components/LevelCreator.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    LevelCreator,
+  },
+});
+</script>
+
+<style>
+  .content {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: stretch;
+    align-content: stretch;
+  }
+
+  .content-section {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ levelcreator/src/components/LevelCreator.vue
@@ -0,0 +1,27 @@
+<template>
+    <canvas ref="canvas"></canvas>
+</template>
+
+<script lang="ts">
+import { ref, computed, onMounted } from "vue";
+import createScene from "../Scenes//LevelCreator"
+
+export default {
+    setup() {
+        const canvas = ref(null);
+        onMounted(() => {
+            createScene(canvas.value); // this is your $el
+        }) 
+        return {
+            canvas
+        }
+    }
+}
+</script>
+
+<style>
+    canvas {
+        width: 100%;
+        height: 100%;
+    }
+</style>
