35a39428e09c9a315470fa987aa24edd70640c59
ringo kam <ringo_kam@hotmail.com>
2020-11-17 15:09:38 -0500

    added a json editor

--- levelcreator/src/App.vue
+++ levelcreator/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="content">
     <div class="content-section">
-      <textarea>
-      </textarea>
+      <JsonEditor />
     </div>
     <div class="content-section">
       <LevelCreator />
@@ -13,11 +12,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import LevelCreator from "./components/LevelCreator.vue";
+import JsonEditor from "./components/JsonEditor.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     LevelCreator,
+    JsonEditor
   },
 });
 </script>
---
+++ levelcreator/src/components/JsonEditor.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <vueJsonEditor class="editor"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// @ts-ignore
+import vueJsonEditor from 'vue-json-editor'
+
+export default defineComponent({
+  components: {
+    vueJsonEditor
+  },
+});
+</script>
+
+<style>
+    .editor {
+        height: 100%;
+    }
+</style>
